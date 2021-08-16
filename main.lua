b4a07d0114eaa769b505d1d5efdc579ba7bd8cdc
Thomas Puckett <30637257+JustASpeedrunner@users.noreply.github.com>
2021-08-16 17:12:47 -0500

    Finish calc, optimize error handling

--- main.lua
+++ main.lua
@@ -1,22 +1,45 @@
-function add(x, y):
+-- Defining functions, such wow
+function add(x, y)
     return x + y
 end
-function subtract(x, y):
+function subtract(x, y)
     return x - y
 end
-function multiply(x, y):
+function multiply(x, y)
     return x * y
 end
-function divide(x, y):
+function divide(x, y)
     return x / y
 end
+local function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+local t = {'1', '2', '3', '4'}
 
-io.write("Choose your operation. 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division")  
+
+-- Taking user input and doing things with it :flushed:
+io.write("Choose your operation. 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division \nEnter your choice here:")  
 local op = io.read()
-io.write("Please enter your first number ")
-local x = io.read()
-io.write("Enter your second number ")
-local y = io.read()
+if has_value(t, op) then
+    io.write("Please enter your first number: ")
+    local x = io.read()
+    io.write("Enter your second number: ")
+    local y = io.read()
 
-if op == "1" then
-    print(x "+" y "=" add(x,y)
+    if op == "1" then
+        print(x, "+", y, "=", add(x,y))
+    elseif op == '2' then
+        print(x, "-", y, "=", subtract(x,y))
+    elseif op == "3" then
+        print(x, "*", y, "=", multiply(x,y))
+    elseif op == "4" then
+        print(x, "/", y, "=", divide(x,y))
+    end
+else
+    print("Invalid operation, try again.")
+end
