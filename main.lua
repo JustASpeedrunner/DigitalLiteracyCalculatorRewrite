d66f0a28d7404de764e61fb9e804b4698cdb2cf3
Thomas Puckett <30637257+JustASpeedrunner@users.noreply.github.com>
2021-08-16 12:31:17 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,15 @@
+function add(x, y):
+    return x + y
+end
+function subtract(x, y):
+    return x - y
+end
+function multiply(x, y):
+    return x * y
+end
+function divide(x, y):
+    return x / y
+end
 
+io.write("Choose your operation. 1) Addition, 2) Subtraction, 3) Multiplication, 4) Division")  
+local op = io.read()
